eda31d5828228ba8e07b4110efbd3d7d45749a83
Taegyun Ko <dev@balsa.to>
2023-03-13 09:35:01 +0900

    gitignore update

--- src/routes/DocRoute.ts
+++ src/routes/DocRoute.ts
@@ -2,16 +2,31 @@ import * as express from 'express';
 
 const DocRoute = express.Router();
 
-DocRoute.get('/:doc', async (request: express.Request, response: express.Response, next: express.NextFunction) =>
+DocRoute.get('/test/:test', async (request: express.Request, response: express.Response, next: express.NextFunction) =>
+{
+  const attributes =
+  {
+    data: request.params.test
+  };
+
+  console.log("request.params.test", request.params.test);
+
+  response.render('index');
+});
+
+DocRoute.get('/*', async (request: express.Request, response: express.Response, next: express.NextFunction) =>
 {
   const attributes =
   {
     data: 1
   };
 
-  console.log(request.url);
+  console.log("request.url", request.url);
+  console.log("request.params", request.params);
 
   response.render('index', attributes);
 });
+
+
 
 export default DocRoute;
